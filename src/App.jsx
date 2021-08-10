a441ee46062435828f5d2106957fd23216ace124
Mher <mhersargsyan2001@gmail.com>
2021-08-10 17:28:20 +0400

    fix(root.css): root.css added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import './root.css';
+import LoginRegisterForm from './components/LoginRegisterForm/LoginRegisterForm';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <LoginRegisterForm />
+    </div>
+  );
 }
 
 export default App;
