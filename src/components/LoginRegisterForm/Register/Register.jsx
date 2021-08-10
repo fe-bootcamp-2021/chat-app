d1f42d681903dcdbd82caa275d7a74b9a2ffbb18
Mher <mhersargsyan2001@gmail.com>
2021-08-10 17:27:22 +0400

    feat(register): component created

---
+++ src/components/LoginRegisterForm/Register/Register.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TelegramLogo } from '../../../img/icons';
+import Input from '../../Input/Input';
+
+function Register() {
+  return (
+    <div className='formContainer'>
+      <div className='logo'>
+        <TelegramLogo />
+      </div>
+      <h1 className='login-form-title'>Sign up to Celegram</h1>
+      <Input type='text' label='Name' />
+      <Input type='text' label='Surname' />
+      <Input type='text' label='Username' />
+      <Input type='email' label='Mail' />
+      <Input type='password' label='Password' />
+      <Input type='password' label='Repeat password' />
+      <button className='login-btn'>Confirm</button>
+      <a>Log in</a>
+    </div>
+  );
+}
+
+export default Register;
