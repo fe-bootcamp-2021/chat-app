e09128f0a65fdc542b6ae4d0a445c99aaa978300
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-08-14 00:01:40 +0400

    feat(src): added formik and yup validation,replacing regular css with a module.css

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -41,7 +41,7 @@ function Register() {
           "Password must contain at least 8 characters, one uppercase, one number and one special case character"
         ),
       repeatPassword: Yup.string()
-        .required("Password is required")
+        .required("Repeat password is required")
         .matches(
           /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
           "Password must contain at least 8 characters, one uppercase, one number and one special case character"
