dfec5c31013a3459b63aa516ee0c04dde5a99190
Mher <mhersargsyan2001@gmail.com>
2021-08-14 19:29:08 +0400

    feat(services/signIn): firebase signIn

---
+++ src/services/signInWithEmailAndPassword.js
@@ -0,0 +1,18 @@
+import firebase from '../firebase';
+
+function signInWithEmailAndPassword({ email, password }) {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+}
+
+export default signInWithEmailAndPassword;
