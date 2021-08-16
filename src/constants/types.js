838d94f46cb7eba6117a8bf4e5e297d661c5917d
Mher <mhersargsyan2001@gmail.com>
2021-08-16 18:56:09 +0400

    fix(types): constants added

---
+++ src/constants/types.js
@@ -0,0 +1,60 @@
+export const types = {
+  input: {
+    password: {
+      name: 'password',
+      type: 'password',
+      id: 'password',
+      label: 'Password',
+      error: {
+        passRequired: 'Password is required',
+        passMinLength: 'Password should be of minimum 8 characters length',
+      },
+    },
+
+    repeatPassword: {
+      name: 'repeatPassword',
+      type: 'password',
+      id: 'repeatPassword',
+      label: 'Repeat password',
+      error: {
+        repeatPassRequired: 'Repeat password is required',
+        passMustMatch: 'Passwords must match',
+      },
+    },
+
+    email: {
+      name: 'email',
+      type: 'email',
+      id: 'email',
+      label: 'Email',
+      error: {
+        validEmail: 'Enter a valid email',
+        emailRequired: 'Email is required',
+      },
+    },
+
+    name: {
+      name: 'name',
+      type: 'text',
+      id: 'name',
+      label: 'Name',
+      error: {
+        nameRequired: 'Name is required',
+        nameMinLength: 'Name must contain at least 2 characters',
+        nameMaxLength: 'Name must contain at least 30 characters',
+      },
+    },
+
+    surname: {
+      name: 'surname',
+      type: 'text',
+      id: 'surname',
+      label: 'Surname',
+      error: {
+        surnameRequired: 'Surname is required',
+        surMinLength: 'Surname must contain at least 2 characters',
+        surnameMaxLength: 'Surname must contain at least 30 characters',
+      },
+    },
+  },
+};
