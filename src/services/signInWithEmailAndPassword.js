b8759bbde31453af851e9fe0002303410d927d6a
Mher <mhersargsyan2001@gmail.com>
2021-08-16 15:28:56 +0400

    fix(services/signin): var rewrited to let

--- src/services/signInWithEmailAndPassword.js
+++ src/services/signInWithEmailAndPassword.js
@@ -6,12 +6,12 @@ function signInWithEmailAndPassword({ email, password }) {
     .signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Signed in
-      var user = userCredential.user;
+      let user = userCredential.user;
       // ...
     })
     .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
+      let errorCode = error.code;
+      let errorMessage = error.message;
     });
 }
 
