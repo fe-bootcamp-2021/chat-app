15550f0b304cc2b2a8eeae1778b0c26fa7e7790e
Mher <mhersargsyan2001@gmail.com>
2021-08-19 15:28:22 +0400

    feat(app.jsx): component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import './root.css';
 import LoginRegisterForm from './components/Auth/Auth';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div>
-      <LoginRegisterForm />
+      {/* <LoginRegisterForm /> */}
+      <Main />
     </div>
   );
 }
