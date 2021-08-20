fafbed01807cca26b204b524c746a8721723c4a5
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-08-20 22:28:47 +0400

    feat(src): Added navbar chat

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBarChat from "../NavBarChat/NavBarChat";
+import classes from "./Chat.module.css";
+
+function Chat() {
+  return (
+    <>
+      <div className={classes.navBarChat}>
+        <NavBarChat />
+      </div>
+    </>
+  );
+}
+
+export default Chat;
--- src/components/NavBarChat/NavBarChat.jsx
+++ src/components/NavBarChat/NavBarChat.jsx
@@ -1,3 +1,24 @@
 import React from "react";
 import classes from "./NavBarChat.module.css";
 
+function NavBarChat() {
+  return (
+    <div className={classes.container}>
+      <div className={classes.chatInfo}>
+        <div className={classes.avatar}>
+          <div className={classes.circle}></div>
+        </div>
+        <div className={classes.context}>
+          <div className={classes.top}></div>
+          <div className={classes.bottom}></div>
+        </div>
+      </div>
+      <div className={classes.chatUtils}>
+        <div className={classes.search}></div>
+        <div className={classes.dotChat}></div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBarChat;
