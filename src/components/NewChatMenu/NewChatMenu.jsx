ba8264d90ad09a2bf4f0504e81a34a305b92a8f4
Mher <mhersargsyan2001@gmail.com>
2021-08-20 18:48:43 +0400

    fix(newChatMenu): component cuted fto newChatMenuRow

--- src/components/NewChatMenu/NewChatMenu.jsx
+++ src/components/NewChatMenu/NewChatMenu.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-import cn from "classnames";
-import Button from "../Button/Button";
-import { Pen, User, Speaker, Group, Close } from "../Icons/icons";
-import { types } from "../../constants/types";
-import classes from "./NewChatMenu.module.css";
+import React, { useState } from 'react';
+import cn from 'classnames';
+import Button from '../Button/Button';
+import { Pen, User, Speaker, Group, Close } from '../Icons/icons';
+import { types } from '../../constants/types';
+import classes from './NewChatMenu.module.css';
+import NewChatMenuRow from '../NewChatMenuRow/NewChatMenuRow';
 
 export default function NewChatMenu() {
   const [isVisible, setIsVisible] = useState(false);
@@ -15,24 +16,9 @@ export default function NewChatMenu() {
   return (
     <div className={classes.wrapper}>
       <div className={cn([classes.container], { [classes.hide]: !isVisible })}>
-        <div className={classes.NewChatMenuRow}>
-          <div className={classes.iconSpeaker}>
-            <Speaker />
-          </div>
-          <span className={classes.testSpeaker}> New Channel </span>
-        </div>
-        <div className={classes.NewChatMenuRow}>
-          <div className={classes.iconGroup}>
-            <Group />
-          </div>
-          <span className={classes.testGroup}> New Group </span>
-        </div>
-        <div className={classes.NewChatMenuRow}>
-          <div className={classes.iconUser}>
-            <User />
-          </div>
-          <span className={classes.testUser}> New Private Chat</span>
-        </div>
+        <NewChatMenuRow rowContent={<Speaker />} rowName={'New Channel'} />
+        <NewChatMenuRow rowContent={<Group />} rowName={'New Group'} />
+        <NewChatMenuRow rowContent={<User />} rowName={'New Private Chat'} />
       </div>
       <Button
         btnName={isVisible ? <Close /> : <Pen />}
