3fcb5248707961cffc040ca082d87d38554b160c
Mher <mhersargsyan2001@gmail.com>
2021-08-23 10:44:37 +0400

    fix(dotMenu): styles fixed

--- src/components/DotMenu/DotMenu.jsx
+++ src/components/DotMenu/DotMenu.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import cn from "classnames";
-import Button from "../Button/Button";
-import { Checked, Delete, Notify, DotMenu } from "../Icons/icons";
-import { types } from "../../constants/types";
-import NewChatMenuRow from "../NewChatMenuRow/NewChatMenuRow";
-import classes from "./DotMenu.module.css";
+import React, { useState } from 'react';
+import cn from 'classnames';
+import Button from '../Button/Button';
+import { Checked, Delete, Notify, DotMenu } from '../Icons/icons';
+import { types } from '../../constants/types';
+import NewChatMenuRow from '../NewChatMenuRow/NewChatMenuRow';
+import classes from './DotMenu.module.css';
 
 function DotMenuChat() {
   const [isVisible, setIsVisible] = useState(false);
@@ -16,18 +16,16 @@ function DotMenuChat() {
   return (
     <>
       <div className={cn([classes.container], { [classes.hide]: !isVisible })}>
-        <NewChatMenuRow rowContent={<Notify />} rowName={"Unmute"}  className={classes.text}/>
-        <NewChatMenuRow rowContent={<Checked />} rowName={"Select Messages"}  className={classes.text}/>
-        <NewChatMenuRow rowContent={<Delete />} rowName={"Delete Chat"} className={classes.textColor}/>
+        <NewChatMenuRow rowContent={<Notify />} rowName={'Unmute'} />
+        <NewChatMenuRow rowContent={<Checked />} rowName={'Select Messages'} />
+        <NewChatMenuRow rowContent={<Delete />} rowName={'Delete Chat'} />
       </div>
-      <div className={classes.icon}>
-        <Button
-          className={classes.iconHover}
-          type={types.button.type}
-          onClick={handleClick}
-          btnName={<DotMenu />}
-        />
-      </div>
+      <Button
+        className={classes.icon}
+        type={types.button.type}
+        onClick={handleClick}
+        btnName={<DotMenu />}
+      />
     </>
   );
 }
