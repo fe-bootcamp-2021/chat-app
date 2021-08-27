5e86d178665abf1ba7b8a0d78c1b4e766a90d0d6
Mher <mhersargsyan2001@gmail.com>
2021-08-27 17:18:57 +0400

    fix(chat.jsx): extra fragment removed

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import NavBarChat from "../NavBarChat/NavBarChat";
-import classes from "./Chat.module.css";
+import React from 'react';
+import NavBarChat from '../NavBarChat/NavBarChat';
+import classes from './Chat.module.css';
 
 function Chat() {
   return (
-    <>
-      <div className={classes.navBarChat}>
-        <NavBarChat />
-      </div>
-    </>
+    <div className={classes.navBarChat}>
+      <NavBarChat />
+    </div>
   );
 }
 
