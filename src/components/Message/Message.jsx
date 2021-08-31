7708a2daca8d0fa2ca28230d64445b3605f4f819
Mher <mhersargsyan2001@gmail.com>
2021-08-31 11:47:33 +0400

    fix(Message): some styles fixed ||constats used

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import messageTypes from '../../constants/messageTypes.constant';
 import classes from './Message.module.css';
 
 function Message({ type }) {
   return (
-    <div className={type === 'mine' ? classes.myMessage : classes.otherUserMessage}>
-      <p className={classes.message}>test</p>
-      <p className={classes.messageTime}>11:11</p>
+    <div className={type === messageTypes.mine ? classes.myBubble : classes.othersBubble}>
+      <div className={type === messageTypes.mine ? classes.myMessage : classes.otherUserMessage}>
+        <p className={classes.message}>test</p>
+        <p className={classes.messageTime}>11:11</p>
+      </div>
     </div>
   );
 }
