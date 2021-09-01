724a294d1748f242221f855d6143bbd66d12b0ad
Mher <mhersargsyan2001@gmail.com>
2021-09-01 11:19:06 +0400

    refactor(*): Sidebar, tochatmenu, auth

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,24 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Login from "../Login/Login";
-import Register from "../Register/Register";
-import Main from "../Main/Main";
-import { routes } from "../../constants/routes.constant";
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import Main from '../Main/Main';
+import { routes } from '../../constants/routes.constant';
 
 function LoginRegisterForm() {
   return (
     <Router>
       <Switch>
-        <Route exact path={routes.login.url}>
-          <Login />
-        </Route>
-        <Route path={routes.register.url}>
-          <Register />
-        </Route>
-        <Route path={routes.home.url}>
-          <Main />
-        </Route>
+        <Route exact path={routes.login.url} component={<Login />} />
+        <Route path={routes.register.url} component={<Register />} />
+        <Route path={routes.home.url} component={<Main />} />
       </Switch>
     </Router>
   );
