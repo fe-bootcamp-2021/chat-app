bee059394040e5ce5145415f4d8b3ceca2bf339a
Mher <mhersargsyan2001@gmail.com>
2021-09-03 13:47:30 +0400

    feat(services): getUser from db func

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -9,6 +9,23 @@ export const addUser = ({ email, name, surname, uid }) => {
   });
 };
 
+export const getUser = () => {
+  return db
+    .ref()
+    .child('users')
+    .get()
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        return { ...snapshot.val() };
+      } else {
+        return 'No data available';
+      }
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
 export const updateProfile = (uid, { description }) => {
   return db.ref('/users/').child(uid).update({
     description,
