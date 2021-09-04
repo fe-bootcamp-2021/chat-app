d22412032bfd5bcb77f41d35ec77858a39e6113d
Mher <mhersargsyan2001@gmail.com>
2021-09-04 15:23:43 +0400

    feat(Settings): component created

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Arrow, Logout, Pen } from '../Icons/icons';
+import Button from '../Button/Button';
+import NewChatMenuRow from '../NewChatMenuRow/NewChatMenuRow';
+import { useAuth } from '../../hooks/useAuth.hook';
+import { useHistory } from 'react-router-dom';
+import routes from '../../constants/routes.constant';
+import classes from './Settings.module.css';
+
+function Settings({ handleSettingsClick }) {
+  const { signout } = useAuth();
+  const history = useHistory();
+
+  const handleSubmit = () => {
+    history.replace(routes.login().route);
+    signout();
+  };
+
+  return (
+    <div className={classes.settingsModal}>
+      <div className={classes.settingsHeader}>
+        <Button
+          btnName={<Arrow />}
+          className={classes.backBtn}
+          onClick={handleSettingsClick}></Button>
+        <Button btnName={<Logout />} onClick={handleSubmit}></Button>
+      </div>
+      <div className={classes.settingsModalAbout}>
+        <div className={classes.circle}></div>
+        <div className={classes.fullName}>Name Surname</div>
+        <div className={classes.email}>namesurname2000@gmail.com</div>
+      </div>
+      <NewChatMenuRow rowContent={<Pen />} rowName={'Edit Profile'} />
+    </div>
+  );
+}
+
+export default Settings;
