d4d98d4ddb54e0d6f8fad6d3577d17d053375870
Mher <mhersargsyan2001@gmail.com>
2021-09-04 17:16:21 +0400

    feat(Settings): EditProfile component added

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Arrow, Logout, Pen } from '../Icons/icons';
 import Button from '../Button/Button';
 import NewChatMenuRow from '../NewChatMenuRow/NewChatMenuRow';
@@ -6,8 +6,11 @@ import { useAuth } from '../../hooks/useAuth.hook';
 import { useHistory } from 'react-router-dom';
 import routes from '../../constants/routes.constant';
 import classes from './Settings.module.css';
+import EditProfile from '../EditProfile/EditProfile';
 
 function Settings({ handleSettingsClick }) {
+  const [isEditProfileOpen, setIsEditProfileOpen] = useState(false);
+
   const { signout } = useAuth();
   const history = useHistory();
 
@@ -16,22 +19,33 @@ function Settings({ handleSettingsClick }) {
     signout();
   };
 
+  const handleEditProfileClick = () => {
+    setIsEditProfileOpen(!isEditProfileOpen);
+  };
+
   return (
-    <div className={classes.settings}>
-      <div className={classes.settingsHeader}>
-        <Button
-          btnName={<Arrow />}
-          className={classes.backBtn}
-          onClick={handleSettingsClick}></Button>
-        <Button btnName={<Logout />} onClick={handleSubmit}></Button>
-      </div>
-      <div className={classes.settingsAbout}>
-        <div className={classes.circle}></div>
-        <div className={classes.fullName}>Name Surname</div>
-        <div className={classes.email}>namesurname2000@gmail.com</div>
-      </div>
-      <NewChatMenuRow rowContent={<Pen />} rowName={'Edit Profile'} />
-    </div>
+    <>
+      {isEditProfileOpen ? (
+        <EditProfile setIsEditProfileOpen={setIsEditProfileOpen} />
+      ) : (
+        <div className={classes.settings}>
+          <div className={classes.settingsHeader}>
+            <Button btnName={<Arrow />} className={classes.backBtn} onClick={handleSettingsClick} />
+            <Button btnName={<Logout />} onClick={handleSubmit} />
+          </div>
+          <div className={classes.settingsAbout}>
+            <div className={classes.circle}></div>
+            <div className={classes.fullName}>Name Surname</div>
+            <div className={classes.email}>namesurname2000@gmail.com</div>
+          </div>
+          <NewChatMenuRow
+            rowContent={<Pen />}
+            rowName={'Edit Profile'}
+            onClick={handleEditProfileClick}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
