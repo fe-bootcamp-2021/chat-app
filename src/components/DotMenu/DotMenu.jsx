f55d0a4b17e996f6244cd44aa69251f13607d51f
Mher <mhersargsyan2001@gmail.com>
2021-09-04 15:26:06 +0400

    fix(DotMenu): Working like normal menu

--- src/components/DotMenu/DotMenu.jsx
+++ src/components/DotMenu/DotMenu.jsx
@@ -1,41 +1,30 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import cn from "classnames";
+import React, { useState } from 'react';
+import cn from 'classnames';
+import Button from '../Button/Button';
+import { Checked, Delete, Notify, DotMenu } from '../Icons/icons';
+import NewChatMenuRow from '../NewChatMenuRow/NewChatMenuRow';
+import { types } from '../../constants/formTypes.constant';
 
-import { useAuth } from "../../hooks/useAuth.hook";
-import Button from "../Button/Button";
-import { Checked, Delete, Notify, DotMenu } from "../Icons/icons";
-import NewChatMenuRow from "../NewChatMenuRow/NewChatMenuRow";
-import { types } from "../../constants/formTypes.constant";
-import routes from "../../constants/routes.constant";
-
-import classes from "./DotMenu.module.css";
+import classes from './DotMenu.module.css';
 
 function DotMenuChat() {
   const [isVisible, setIsVisible] = useState(false);
-  const { signout } = useAuth();
-  const history = useHistory();
 
-  // const handleClick = () => { // zhamanakavor popoxutyun
-  //   setIsVisible(!isVisible);
-  // };
-
-  const handleSubmit = () => {
-    history.replace(routes.login().route);
-    signout();
+  const handleClick = () => {
+    setIsVisible(!isVisible);
   };
 
   return (
     <>
       <div className={cn([classes.container], { [classes.hide]: !isVisible })}>
-        <NewChatMenuRow rowContent={<Notify />} rowName={"Unmute"} />
-        <NewChatMenuRow rowContent={<Checked />} rowName={"Select Messages"} />
-        <NewChatMenuRow rowContent={<Delete />} rowName={"Delete Chat"} />
+        <NewChatMenuRow rowContent={<Notify />} rowName={'Unmute'} />
+        <NewChatMenuRow rowContent={<Checked />} rowName={'Select Messages'} />
+        <NewChatMenuRow rowContent={<Delete />} rowName={'Delete Chat'} />
       </div>
       <Button
         className={classes.icon}
         type={types.button.type}
-        onClick={handleSubmit}
+        onClick={handleClick}
         btnName={<DotMenu />}
       />
     </>
