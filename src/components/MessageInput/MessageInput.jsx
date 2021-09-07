03813319c4162c4eb3d2fd67b5f5e92e896843cc
Mher <mhersargsyan2001@gmail.com>
2021-09-07 20:44:12 +0400

    feat(MessageInput): Smile is working

--- src/components/MessageInput/MessageInput.jsx
+++ src/components/MessageInput/MessageInput.jsx
@@ -1,22 +1,51 @@
-import React, { useState } from 'react';
-
+import React, { createRef, useState } from 'react';
+import Picker from 'emoji-picker-react';
 import FileUpload from '../FileUpload/FileUpload';
 import SendMessage from '../SendMessage/SendMessage';
 import { types } from '../../constants/formTypes.constant';
-
+import { Smile } from '../Icons/icons';
+import Button from '../Button/Button';
 import classes from './MessageInput.module.css';
 
 function MessageInput({ onSend }) {
+  const inputRef = createRef();
   const [message, setMessage] = useState('');
+  const [isEmojiOpen, setIsEmojiOpen] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState();
+
+  const pickEmoji = (e, { emoji }) => {
+    const ref = inputRef.current;
+    ref.focus();
+    const start = message.substring(0, ref.selectionStart);
+    const end = message.substring(ref.selectionStart);
+    const text = start + emoji + end;
+    setMessage(text);
+    setCursorPosition(start.length + emoji.length);
+  };
+
+  const handleEmojiButton = () => {
+    setIsEmojiOpen(!isEmojiOpen);
+  };
+
+  const handleInputChange = (e) => {
+    setMessage(e.target.value);
+  };
 
   return (
     <div className={classes.messageInput}>
+      <div className={classes.smileIcon}>
+        <Button btnName={<Smile />} onClick={handleEmojiButton} />
+        <div className={isEmojiOpen ? classes.smileMenu : classes.smileMenuClose}>
+          <Picker onEmojiClick={pickEmoji} pickerStyle={{ border: 'none', boxShadow: 'none' }} />
+        </div>
+      </div>
       <input
         type={types.input.message.typ}
         placeholder={types.input.message.placeholder}
         className={classes.input}
-        onChange={(e) => setMessage(e.target.value)}
+        onChange={handleInputChange}
         value={message}
+        ref={inputRef}
       />
       <SendMessage
         onClick={() => {
