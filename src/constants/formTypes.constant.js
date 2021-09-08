65c892ac66bb8b68e6d2c6b849ebeb414c88e313
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-08 13:06:43 +0400

    feat(constants): newPassword and repeat new password

--- src/constants/formTypes.constant.js
+++ src/constants/formTypes.constant.js
@@ -1,81 +1,103 @@
 export const types = {
   input: {
     message: {
-      type: 'text',
-      placeholder: 'message',
+      type: "text",
+      placeholder: "message",
     },
 
     file: {
-      type: 'file',
-      id: 'file-input',
+      type: "file",
+      id: "file-input",
     },
 
     search: {
-      type: 'search',
-      id: 'search',
-      label: 'Search',
-      name: 'Search',
-    },
-
-    password: {
-      name: 'password',
-      type: 'password',
-      id: 'password',
-      label: 'Password',
-      error: {
-        passRequired: 'Password is required',
-        passMinLength: 'Password should be of minimum 8 characters length',
-      },
-    },
-
-    repeatPassword: {
-      name: 'repeatPassword',
-      type: 'password',
-      id: 'repeatPassword',
-      label: 'Repeat password',
-      error: {
-        repeatPassRequired: 'Repeat password is required',
-        passMustMatch: 'Passwords must match',
-      },
-    },
-
-    email: {
-      name: 'email',
-      type: 'email',
-      id: 'email',
-      label: 'Email',
-      error: {
-        validEmail: 'Enter a valid email',
-        emailRequired: 'Email is required',
-      },
+      type: "search",
+      id: "search",
+      label: "Search",
+      name: "Search",
     },
 
     name: {
-      name: 'name',
-      type: 'text',
-      id: 'name',
-      label: 'Name',
+      name: "name",
+      type: "text",
+      id: "name",
+      label: "Name",
       error: {
-        nameRequired: 'Name is required',
-        nameMinLength: 'Name must contain at least 2 characters',
-        nameMaxLength: 'Name must contain at least 30 characters',
+        nameRequired: "Name is required",
+        nameMinLength: "Name must contain at least 2 characters",
+        nameMaxLength: "Name must contain at least 30 characters",
       },
     },
 
     surname: {
-      name: 'surname',
-      type: 'text',
-      id: 'surname',
-      label: 'Surname',
+      name: "surname",
+      type: "text",
+      id: "surname",
+      label: "Surname",
       error: {
-        surnameRequired: 'Surname is required',
-        surMinLength: 'Surname must contain at least 2 characters',
-        surnameMaxLength: 'Surname must contain at least 30 characters',
+        surnameRequired: "Surname is required",
+        surMinLength: "Surname must contain at least 2 characters",
+        surnameMaxLength: "Surname must contain at least 30 characters",
+      },
+    },
+
+    email: {
+      name: "email",
+      type: "email",
+      id: "email",
+      label: "Email",
+      error: {
+        validEmail: "Enter a valid email",
+        emailRequired: "Email is required",
+      },
+    },
+
+    password: {
+      name: "password",
+      type: "password",
+      id: "password",
+      label: "Password",
+      error: {
+        passRequired: "Password is required",
+        passMinLength: "Password should be of minimum 8 characters length",
+      },
+    },
+
+    newPassword: {
+      name: "password",
+      type: "password",
+      id: "password",
+      label: "New Password",
+      error: {
+        passRequired: "Password is required",
+        passMinLength: "Password should be of minimum 8 characters length",
+      },
+    },
+
+    repeatPassword: {
+      name: "repeatPassword",
+      type: "password",
+      id: "repeatPassword",
+      label: "Repeat password",
+      error: {
+        repeatPassRequired: "Repeat password is required",
+        passMustMatch: "Passwords must match",
+      },
+    },
+
+    repeatNewPassword: {
+      name: "repeatPassword",
+      type: "password",
+      id: "repeatPassword",
+      label: "Repeat new password",
+      error: {
+        repeatPassRequired: "Repeat password is required",
+        passMustMatch: "Passwords must match",
       },
     },
   },
   button: {
-    type: 'submit',
-    name: 'Confirm',
+    type: "submit",
+    name: "Confirm",
   },
 };
