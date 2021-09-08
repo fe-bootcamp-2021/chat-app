8dd1ecd06f2fc16264c26c32fceb1ecc87523e07
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-08 13:05:15 +0400

    feat(helpers): Added validation in edit profile

--- src/helpers/formValidation.helper.js
+++ src/helpers/formValidation.helper.js
@@ -1,36 +1,36 @@
 import * as Yup from "yup";
-import { types } from '../constants/formTypes.constant';
+import { types } from "../constants/formTypes.constant";
 
 const validationLogin = Yup.object({
-    email: Yup.string()
-      .email(types.input.email.error.validEmail)
-      .required(types.input.email.error.emailRequired),
-    password: Yup.string()
-      .required(types.input.password.error.passRequired)
-      .min(8, types.input.password.error.passMinLength),
-  })
+  email: Yup.string()
+    .email(types.input.email.error.validEmail)
+    .required(types.input.email.error.emailRequired),
+  password: Yup.string()
+    .required(types.input.password.error.passRequired)
+    .min(8, types.input.password.error.passMinLength),
+});
 
 const validationSignUp = Yup.object({
-    name: Yup.string()
-      .max(30, types.input.name.error.nameMaxLength)
-      .required(types.input.name.error.nameRequired)
-      .min(2, types.input.name.error.nameMinLength),
-    surname: Yup.string()
-      .max(30, types.input.surname.error.surnameMaxLength)
-      .required(types.input.surname.error.surnameRequired)
-      .min(2, types.input.surname.error.surMinLength),
-    email: Yup.string()
-      .email(types.input.email.error.validEmail)
-      .required(types.input.email.error.emailRequired),
-    password: Yup.string()
-      .required(types.input.password.error.passRequired)
-      .min(8, types.input.password.error.passMinLength),
-    repeatPassword: Yup.string()
-      .required(types.input.repeatPassword.error.repeatPassRequired)
-      .oneOf(
-        [Yup.ref(types.input.password.id), null],
-        types.input.repeatPassword.error.passMustMatch
-      ),
-  })
+  name: Yup.string()
+    .max(30, types.input.name.error.nameMaxLength)
+    .required(types.input.name.error.nameRequired)
+    .min(2, types.input.name.error.nameMinLength),
+  surname: Yup.string()
+    .max(30, types.input.surname.error.surnameMaxLength)
+    .required(types.input.surname.error.surnameRequired)
+    .min(2, types.input.surname.error.surMinLength),
+  email: Yup.string()
+    .email(types.input.email.error.validEmail)
+    .required(types.input.email.error.emailRequired),
+  password: Yup.string()
+    .required(types.input.password.error.passRequired)
+    .min(8, types.input.password.error.passMinLength),
+  repeatPassword: Yup.string()
+    .required(types.input.repeatPassword.error.repeatPassRequired)
+    .oneOf(
+      [Yup.ref(types.input.password.id), null],
+      types.input.repeatPassword.error.passMustMatch
+    ),
+});
 
-  export {validationSignUp,validationLogin}
+export { validationSignUp, validationLogin };
